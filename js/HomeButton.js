5959b2fa6581072d9ceb076b65302b6d30c4d27d
Matt Driscoll <matthew.driscoll@gmail.com>
2013-11-11 18:09:09 -0800

    on.once

--- js/HomeButton.js
+++ js/HomeButton.js
@@ -75,7 +75,7 @@ function (
             if (this.map.loaded) {
                 this._init();
             } else {
-                on(this.map, "load", lang.hitch(this, function() {
+                on.once(this.map, "load", lang.hitch(this, function() {
                     this._init();
                 }));
             }
